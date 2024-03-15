132bbf2cbf818010a5db32f96d1977edf45a996c
Tusnelde Endjala <tusneldelainae@gmail.com>
2024-03-15 02:12:04 +0200

    Technical assignment

---
+++ Ass.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Demonstrating int data type
+  int age = 30;
+  print('Age: $age');
+
+  // Demonstrating double data type
+  double height = 1.6;
+  print('Height: $height feet');
+
+  // Demonstrating String data type
+  String name = 'Tusnelde Endjala';
+  print('Name: $name');
+
+  // Demonstrating List data type (List of integers)
+  List<int> numbers = [2021, 2022, 2023, 2024, 2025];
+  print('Numbers: $numbers');
+
+  // Demonstrating List data type (List of strings)
+  List<String> fruits = ['Apple', 'Mango', 'Orange'];
+  print('Fruits: $fruits');
+
+  // Demonstrating Map data type (Map of String keys and int values)
+  Map<String, int> studentScores = {'Math': 90, 'Science': 85, 'English': 88};
+  print('Student Scores: $studentScores');
+}
